84a522db8b16d470a3883368c03e64981b2bec90
VANDO MONTEIRO <vando.monteiro@etec.sp.gov.br>
2022-04-04 12:39:17 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_simulado_samaris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtde;
+            int maior = 0;
+            int total = 0;
+            int num = 0;
+            int contador = 0; // int n = 0; no da professora
+
+            Console.Write("Digite a quantidade de entradas: ");
+            qtde = Convert.ToInt32(Console.ReadLine());
+                       
+            
+            while (contador < qtde)
+            {
+                Console.Write("Digite um número inteiro: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                total = total + num;
+                contador++;
+
+                if (num > maior)
+                {
+                    maior = num;
+                }
+            }
+
+            Console.Write("Total: " + total + "\nMaior valor: " + maior);
+            Console.ReadLine();
+
+        }
+    }
+}
